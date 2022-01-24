dbddbb7af332eac927b0be8507e37554cde07e4e
arthur0010 <94835248+arthur0010@users.noreply.github.com>
2022-01-24 13:35:30 +0330

    Update and rename cod-filtering-keristofer to cod-filtering-keristofer.py

---
+++ cod-filtering-keristofer.py
@@ -0,0 +1,70 @@
+print("*keristofer*")
+print('pedar filtering')
+import os
+#What do you have in the source?
+os.system("pip install colored")
+os.system("pip install colorama")
+os.system("pip install datetime")
+#importing
+import time
+import sys
+import colored
+import colorama
+import datetime
+from colored import fg, bg, attr
+from colorama import Fore, Back, Style
+# colors
+red='\033[31m'
+green='\033[32m'
+blue='\033[36m'
+pink='\033[35m'
+rang='\033[34m'
+os.system("clear")
+print(f"{blue} ")
+print(' aqa amir (keristofer) ')
+print('         Ａ Ｍ Ｉ Ｒ   ')
+print(f"{pink} ")
+print("""                    ( ~ ~ )
+                    ( ▪︎  ▪︎)
+                    ( ___ )
+	        ____/      \ ____
+	        |               |               
+	        |               |
+	        |               |_________
+	       -------------------AMIR----- """)
+print(f"{red} ")
+up = input(" account 1 × gap 2 × chanel 3 >> ")
+if up == "1": 
+            print("account = (3.5.3.6.5.4.2 2.4.5.fil-fil-rubika.55.8.0.1.3.2.3.4.3.com")
+if up != "1":
+            print(' cod account ')
+if up == "2":
+            print("gap = (2.3.4.4.6.7.8.8.6.5.2.2.3.5.6.6.7.8.8.7.5.4.2.2.FILTER-FILTER.2.4.5.6.0.8.7.4.2.1.filter.2.4.5.5.4.3.2.2.4.5.5.6.0.8.7.4.filtering-rubika.2.4.6.5.4.2.4.5.6.6.4.3.fil//com.https://s4.uupload.ir/files/img_20210728_194736_412_9t6h.jpg.16.33.20.000.filtering-rubika(16.33.19.000)")
+if up != "2":
+            print(' cod gap ')
+if up == "3":
+            print(" chanel = (4.2.3.4.0.0.1.3.4./h//d.6.5.4.3.2.4.5.2.4.4.3.2/)")
+if up != "3":
+            print(' cod chanel ')
+time.sleep(0.3)
+print('kod ro 10 bar bezanid ke feik nashe')
+print('♡Thanks♡')
+print('aqa amir')
+print('*keristofer*')
+print(f"{blue} ")
+print("""%%%%%%%%%%
+%%%%%%%%%
+%%%%%%%%
+%%%%%%%
+%%%%%%
+%%%%%
+%%%%
+%%%
+%%
+% """)
+print(f"{green} ")
+print(" chanel rubika = @aqa_Christopher ")
+print(" zendegim bita ")
+print(f"{red} ")
+print("       abar script [cod filtering] ")
+print("      bay     ")
