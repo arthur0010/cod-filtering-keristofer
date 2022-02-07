096dddf65c5640f379fcd0b2891d9038b93613d4
arthur0010 <94835248+arthur0010@users.noreply.github.com>
2022-02-07 11:29:14 +0330

    Update cod-filtering-keristofer.py

--- cod-filtering-keristofer.py
+++ cod-filtering-keristofer.py
@@ -24,26 +24,44 @@ print(f"{blue} ")
 print(' aqa amir (keristofer) ')
 print('         Ａ Ｍ Ｉ Ｒ   ')
 print(f"{pink} ")
-print("""                    ( ~ ~ )
-                    ( ▪︎  ▪︎)
-                    ( ___ )
-	        ____/      \ ____
-	        |               |               
-	        |               |
-	        |               |_________
-	       -------------------AMIR----- """)
+print("""
+
+n                     .             .                n.
+  . .dP                   dP               9b               9b   .
+4  qXb         .        dX                 Xb       .      dXp   t
+dX.  9Xb      .dXb                              dXb.   dXP   .Xb
+9XXb._     _.dXXXXb dXXXXbo.               dXXXXb dXXXXb._   _.dXXP
+9XXXXXXXXXXXXXVXXXXXXXXOo.           .oOXXXXXXXXVXXXXXXXXXXXXXXXP
+   9XXXXXXXXXXXXXXX ~   ~ OOO8b   d8OOO ~   ~ XXXXXXXXXXXXXXXXXP
+     9XXXXXP   9XX      *     98v8P      *     XXP   9XXXXXXXP
+      ~~~       9X.          .db|db.          .XP       ~~~
+                  )b.  .dbo.dP' v  9b.odb.  .dX(
+                ,dXXXXXXXXXXXb     dXXXXXXXXXXXb.
+               dXXXXXXXXXXXP'   .    9XXXXXXXXXXXb
+              dXXXXXXXXXXXXb   d|b   dXXXXXXXXXXXXb
+              9XXb'    XXXXXb.dX|Xb.dXXXXX'    dXXP
+                       9XXXXXX(   )XXXXXXP
+                       XXXX X. v .X XXXX
+                        XP^X' b   d' X^XX
+                        X. 9         P )X
+                         b          '  d'
+
+
+                        AMIR
+
+""")
 print(f"{red} ")
 up = input(" account 1 × gap 2 × chanel 3 >> ")
 if up == "1": 
-            print("account (af.af.af.af/report.https://5.4.2.5.5.4.2.1.4.(9.2-8).6.4.2./23:42:26.683/(0.0.1-8)server*(0.98)1400-filter.rubika-bug-bug.ir")
+            print("account = https://6.4.6.4.2.4.6.6.4.2.2.5.3.2.4.7.6-0.6.2.6.3.fil-bug.com/yfttk15/xnxx.com/server*(0.3.6)filtering.rubika.com")
 if up != "1":
             print(' cod account ')
 if up == "2":
-            print("gap = (https://s4.uupload.ir/files/img_20210728_194732_694_vlkn.jpg.filtering.https://5.4.2.4.5.4.2.2.4.4.2.(3.1.8/)code.kode(yfttk15)rubika.fil.fil.com")
+            print("gap = https://filter-chenal.bug.5.2.1.4.0.5.2.1.5.3.2.5.3.2.4.//com.target-https://rubika.ir.filtering.com")
 if up != "2":
             print(' cod gap ')
 if up == "3":
-            print(" chanel = (https://bug-server.0.3.2.4.3.2.1.3.4 3.2.1.3.4.2.5.3.2.4.ir*https://ter.af-ter.af-ter.*.kode.code. (/YtTk_)(۳.۱.۶.۸/)rubika.fill-robika.fyll.*(8.7-0)")
+            print(" chanel = ")
 if up != "3":
             print(' cod chanel ')
 time.sleep(0.3)
