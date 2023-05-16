b1a3a4831fc1933419a5507cf06d03c9e5645b25
arthur0010 <94835248+arthur0010@users.noreply.github.com>
2023-05-16 22:16:53 +0330

    Update cod-filtering-keristofer.py

--- cod-filtering-keristofer.py
+++ cod-filtering-keristofer.py
@@ -1,149 +1,259 @@
-print("Ａ Ｍ Ｉ Ｒ")
-print(" **keristofer** ")
-print("  pedar filtering  ")
-import os
-#What do you have in the source?
-os.system("pip install colored")
-os.system("pip install colorama")
-os.system("pip install datetime")
-#importing
-import time
-import sys
-import colored
-import colorama
-import datetime
-from colored import fg, bg, attr
-from colorama import Fore, Back, Style
-# colors
-red='\033[31m'
-green='\033[32m'
-blue='\033[36m'
-pink='\033[35m'
-rang='\033[34m'
-os.system("clear")
-print(f"{red} ")
-print(Fore.WHITE + "")
-#date
-x = str(datetime.datetime.now())
-print("started " + (x))
-print(f"{green} ")
-print(" cod filtering ")
-print(Fore.RED + "")
-x = """ Loading ~ ~ ~
+from os import system
 
-                ..:::::::::..
-           ..:::aad8888888baa:::..
-        .::::d:?88888888888?::8b::::.
-      .:::d8888:?88888888??a888888b:::.
-    .:::d8888888a8888888aa8888888888b:::.
-   ::::dP::::::::88888888888::::::::Yb::::
-  ::::dP:::::::::Y888888888P:::::::::Yb::::
- ::::d8:::::::::::Y8888888P:::::::::::8b::::
-.::::88::::::::::::Y88888P::::::::::::88::::.
-:::::Y8baaaaaaaaaa88P:T:Y88aaaaaaaaaad8P:::::                                           :::::::Y88888888888P::|::Y88888888888P:::::::
-::::::::::::::::888:::|:::888::::::::::::::::
-`:::::::::::::::8888888888888b::::::::::::::'
- :::::::::::::::88888888888888::::::::::::::
-  :::::::::::::d88888888888888:::::::::::::
-   ::::::::::::88::88::88:::88::::::::::::
-    `::::::::::88::88::88:::88::::::::::'
-      `::::::::88::88::88:::88::::::::'
-        `::::::88::88::88:::88::::::'
-           ``:::::::::::::::::::''
-                ``:::::::::::
+try:
 
+	from datetime import datetimeexcept:
 
-              K E R I S T O F E R 
+	system("pip install datetime")
 
+try:
 
-"""
-for c in x:
-        sys.stdout.write(c)
-        sys.stdout.flush()
-        time.sleep(0.003)
-print(f"{blue} ")
-print("#.........")
-time.sleep(0.9)
-print("##........")
-time.sleep(0.9)
-print("###.......")
-time.sleep(0.8)
-print("####......")
-time.sleep(0.7)
-print("#####.....")
-time.sleep(0.6)
-print("######....")
-time.sleep(0.5)
-print("#######...")
-time.sleep(0.4)
-print("########..")
-time.sleep(0.3)
-print("#########.")
-time.sleep(0.2)
-print("##########")
-time.sleep(0.1)
-print(f"{pink} ")
-print("Play - - - ! ")
-print(Fore.YELLOW + "")
-time.sleep(1.7)
-print("  abar filtering (keristofer) ")
-print(" ")
-print("   chanel rubika = @aqa_keristofer  ")
-print(" ")
-print(f"{green} ")
-print(" cod server ")
-time.sleep(0.1)
-print("server1 = y//d/f")
-time.sleep(1.0)
-print("server2 = g/d//")
-time.sleep(0.1)
-print(" Dxprit ")
-time.sleep(1.0)
-print("Dxprit = http://Dxprit.nob.hackers6683648249.76.infect.in ")
-time.sleep(0.1)
-print(f"{red} ")
-up = input("""
+	from random import choice,randint
 
-《 cod filter account 1 》
+except:
 
-《 cod filter gap 2 》
+	system("pip install random")
 
-《 cod filter chanel 3 》
+try:
 
- Which do you want >>> """)
-print("  ")
-print(f"{blue} ")
-time.sleep(2.0)
-if up == "1":
-        print(' cod account = (4.9.3.1///u////y.7.7.4.1.1.5.7.0.8.6.4.2.2.6.7.2.fil.rubika.ir)')
-if up != "1":
-        print(' cod filter account ')
-if up == "2":
-        print(' cod gap = (fil.fil.ter.af.2.4.8.9.8.6.4.2.1.5.7.9.9.7.6.4.2/port:80.9.8.6.4/)')
-if up != "2":
-        print(' cod filter gap ')
-if up == "3":
-        print(' cod chanel = (report.fil.gif.sxs.com/(/yftt15k)filter.rubika.phone.sxs.com[CFC+BUG]) ')
-if up != "3":
-        print(' cod filter chanel ')
-time.sleep(0.6)
-print("""
+	from rainbowtext import text
 
-         cod haro mahklot konid !
+except:
+
+	system("pip install rainbowtext")
+
+try:
+
+	from time import sleep
+
+except:
+
+	system("pip install time")
+
+system("clear")
+
+green="\033[1;92m"
+
+red ="\033[1;91m"
+
+blue="\033[1;94m"
+
+pink="\033[1;95m"
+
+yellow="\033[1;93m"
+
+white="\033[1;00m"
+
+OF = "\033[0m"
+
+ww = "\033[0;100m"
+
+server = ["/y//d/f/","/f//d/","/d//f/h/","/f//a/y/","/e////f.h/","/f/h.u//","/f////g.h/","/g//d/","/f/h.g//","/r//g/h"]
+
+cod = ["1","2","3","4","5","6","7","8","9","0"]
+
+code = ("("+choice(cod)+"."+choice(cod)+"."+choice(cod)+"."+choice(cod)+choice(server)+choice(cod)+"."+choice(cod)+"."+choice(cod)+"."+choice(cod)+"."+choice(cod)+"."+choice(cod)+"."+choice(cod)+"."+choice(cod)+"."+choice(cod)+"."+choice(cod)+"."+choice(cod)+"."+choice(cod)+"."+choice(cod)+"."+choice(cod)+"."+choice(cod)+"."+choice(cod)+"."+choice(cod)+"."+choice(cod)+"."+choice(cod)+"."+choice(cod)+"."+choice(cod)+"."+choice(cod)+"."+choice(cod)+"."+choice(cod)+"."+choice(cod)+"."+choice(cod)+"."+choice(cod)+"."+choice(cod)+"."+choice(cod)+"."+choice(cod)+"."+choice(cod)+"."+choice(cod)+"."+choice(cod)+"."+choice(cod)+"."+choice(cod)+"."+choice(cod)+"."+choice(cod)+"/)")
+
+link_sexy = ["https://s8.uupload.ir/files/screenshot_۲۰۲۳۰۵۱۶_۱۶۲۵۳۱_v8p.jpg","https://s8.uupload.ir/files/screenshot_۲۰۲۳۰۵۱۶_۱۶۲۸۳۱_u05n.jpg","https://s8.uupload.ir/files/screenshot_۲۰۲۳۰۵۰۸_۱۴۵۰۱۴_uot3.jpg","https://s8.uupload.ir/files/crdghzedz1lx_6ew0.jpg"]
+
+n = 1
+
+r = "="
+
+while (n <= 100):
+
+	print(r,f"%{n}")
+
+	n = n + randint(1,5)
+
+	r = r + "="
+
+	sleep(000000.1)
+
+	
+
+sleep(3)
+
+system("clear")
+
+print(f"""{white}
+
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⡠⢤⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⡴⠟⠃⠀⠀⠙⣄⠀⠀⠀⠀⠀⠀⠀⠀⠀
+
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣠⠋⠀⠀⠀⠀⠀⠀⠘⣆⠀⠀⠀⠀⠀⠀⠀⠀
+
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢠⠾⢛⠒⠀⠀⠀⠀⠀⠀⠀⢸⡆⠀⠀⠀⠀⠀⠀⠀
+
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣿⣶⣄⡈⠓⢄⠠⡀⠀⠀⠀⣄⣷⠀⠀⠀⠀⠀⠀⠀
+
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣿⣷⠀⠈⠱⡄⠑⣌⠆⠀⠀⡜⢻⠀⠀⠀⠀⠀⠀⠀
+
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⣿⡿⠳⡆⠐⢿⣆⠈⢿⠀⠀⡇⠘⡆⠀⠀⠀⠀⠀⠀
+
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢿⣿⣷⡇⠀⠀⠈⢆⠈⠆⢸⠀⠀⢣⠀⠀⠀⠀⠀⠀
+
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠘⣿⣿⣿⣧⠀⠀⠈⢂⠀⡇⠀⠀⢨⠓⣄⠀⠀⠀⠀
+
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣸⣿⣿⣿⣦⣤⠖⡏⡸⠀⣀⡴⠋⠀⠈⠢⡀⠀⠀
+
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢠⣾⠁⣹⣿⣿⣿⣷⣾⠽⠖⠊⢹⣀⠄⠀⠀⠀⠈⢣⡀
+
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡟⣇⣰⢫⢻⢉⠉⠀⣿⡆⠀⠀⡸⡏⠀⠀⠀⠀⠀⠀⢇
+
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢨⡇⡇⠈⢸⢸⢸⠀⠀⡇⡇⠀⠀⠁⠻⡄⡠⠂⠀⠀⠀⠘
+
+⢤⣄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢠⠛⠓⡇⠀⠸⡆⢸⠀⢠⣿⠀⠀⠀⠀⣰⣿⣵⡆⠀⠀⠀⠀
+
+⠈⢻⣷⣦⣀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣠⡿⣦⣀⡇⠀⢧⡇⠀⠀⢺⡟⠀⠀⠀⢰⠉⣰⠟⠊⣠⠂⠀⡸
+
+⠀⠀⢻⣿⣿⣷⣦⣀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣠⢧⡙⠺⠿⡇⠀⠘⠇⠀⠀⢸⣧⠀⠀⢠⠃⣾⣌⠉⠩⠭⠍⣉⡇
+
+⠀⠀⠀⠻⣿⣿⣿⣿⣿⣦⣀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣠⣞⣋⠀⠈⠀⡳⣧⠀⠀⠀⠀⠀⢸⡏⠀⠀⡞⢰⠉⠉⠉⠉⠉⠓⢻⠃
+
+⠀⠀⠀⠀⠹⣿⣿⣿⣿⣿⣿⣷⡄⠀⠀⢀⣀⠠⠤⣤⣤⠤⠞⠓⢠⠈⡆⠀⢣⣸⣾⠆⠀⠀⠀⠀⠀⢀⣀⡼⠁⡿⠈⣉⣉⣒⡒⠢⡼⠀
+
+⠀⠀⠀⠀⠀⠘⣿⣿⣿⣿⣿⣿⣿⣎⣽⣶⣤⡶⢋⣤⠃⣠⡦⢀⡼⢦⣾⡤⠚⣟⣁⣀⣀⣀⣀⠀⣀⣈⣀⣠⣾⣅⠀⠑⠂⠤⠌⣩⡇⠀
+
+⠀⠀⠀⠀⠀⠀⠘⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡁⣺⢁⣞⣉⡴⠟⡀⠀⠀⠀⠁⠸⡅⠀⠈⢷⠈⠏⠙⠀⢹⡛⠀⢉⠀⠀⠀⣀⣀⣼⡇⠀
+
+⠀⠀⠀⠀⠀⠀⠀⠀⠈⠻⣿⣿⣿⣿⣿⣿⣿⣿⣽⣿⡟⢡⠖⣡⡴⠂⣀⣀⣀⣰⣁⣀⣀⣸⠀⠀⠀⠀⠈⠁⠀⠀⠈⠀⣠⠜⠋⣠⠁⠀
+
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠙⢿⣿⣿⣿⡟⢿⣿⣿⣷⡟⢋⣥⣖⣉⠀⠈⢁⡀⠤⠚⠿⣷⡦⢀⣠⣀⠢⣄⣀⡠⠔⠋⠁⠀⣼⠃⠀⠀
+
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠻⣿⣿⡄⠈⠻⣿⣿⢿⣛⣩⠤⠒⠉⠁⠀⠀⠀⠀⠀⠉⠒⢤⡀⠉⠁⠀⠀⠀⠀⠀⢀⡿⠀⠀⠀
+
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠙⢿⣤⣤⠴⠟⠋⠉⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠑⠤⠀⠀⠀⠀⠀⢩⠇⠀⠀⠀
+
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
 
 """)
-print(f"{pink} ")
-print(" cod ro 10 bar bezanid ke feik nashe ")
-print("♡Thanks♡")
-print(" ip rubika = 5.106.10.226 ")
-print(f"{red} ")
-print(" no love ")
-print(" ")
-print(" abar filtering rubika aqa amir ( keristofer ) ")
-time.sleep(2.0)
-print(""" Update Kardan script 👇:
-    	
-    	
+
+sleep(1)
+
+print("")
+
+print(f"            {red}✪ {green}START TIME --->",datetime.now().strftime('%Y-%m-%d  %H:%M:%S'),f"{red}✪ ")
+
+sleep(1)
+
+print("")
+
+print("")
+
+print(f"{yellow}―――‣{pink} im keristofer")
+
+sleep(0.4)
+
+print(f"{yellow}―――――‣{pink} rekord dar filtering ")
+
+sleep(0.3)
+
+print(f"{yellow}―――――――‣{pink} 09189256026")
+
+sleep(0.2)
+
+print("")
+
+print(f"""{blue}
+
+          _             _     _         __
+
+         | | _____ _ __(_)___| |_ ___  / _| ___ _ __
+
+         | |/ / _ \ '__| / __| __/ _ \| |_ / _ \ '__|
+
+         |   <  __/ |  | \__ \ || (_) |  _|  __/ |
+
+         |_|\_\___|_|  |_|___/\__\___/|_|  \___|_|
+
+""")
+
+print("")
+
+print(f"{red}[{green}1{red}] {yellow}―――‣ {red}code account ")
+
+print(f"{red}[{green}2{red}] {yellow}―――‣ {red}code group ")
+
+print(f"{red}[{green}3{red}] {yellow}―――‣ {red}code channel ")
+
+print(f"{red}[{green}4{red}] {yellow}―――‣ {red}link sexy ")
+
+print(f"{red}[{green}5{red}] {yellow}―――‣ {red}link mokharab ")
+
+print(f"{red}[{green}6{red}] {yellow}―――‣ {red}code hasas be server ")
+
+print(f"{red}[{green}7{red}] {yellow}―――‣ {red}code tor ")
+
+print(f"{red}[{green}8{red}] {yellow}―――‣ {red}link virus ")
+
+print(f"{red}[{green}9{red}] {yellow}―――‣ {red}ip rubika ")
+
+print("")
+
+ii = int(input(f"{red}[{green}?{red}] {green}Please enter the desired option --->{white} "))
+
+sleep(2)
+
+print("")
+
+print("")
+
+if ii == 1:
+
+	print(f"{blue} code account : {ww}{code} ",OF)
+
+if ii == 2:
+
+	print(f"{blue} code group : {ww}{code} ",OF)
+
+if ii == 3:
+
+	print(f"{blue} code channel : {ww}{code} ",OF)
+
+if ii == 4:
+
+	print(f"{blue} link sexy : {ww}{(choice(link_sexy))} ",OF)
+
+if ii == 5:
+
+	print(f"{blue} link mokharab : {ww}https://bit.ly/3ild93L ",OF)
+
+if ii == 6:
+
+	print(f"{blue} code hasas be server : {ww}(Server.onlline/server.offline.onlline/)",OF)
+
+tor = datetime.now().strftime('%d  %H:%M:%S')
+
+if ii == 7:
+
+	print(f"{blue} code tor : {ww}{tor} ",OF)
+
+if ii == 8:	
+
+	print(f"{blue} link virus : {ww}http://Hackweb.xyz/python.html.cs ",OF)
+
+if ii == 9:
+
+	print(f"{blue} ip rubika : {ww}5.106.8.151 ",OF)
+
+sleep(3)
+
+print("")
+
+print("")
+
+print(f"                  \033[0;102m {red} makhlot konid bad bezanid{OF}")
+
+sleep(1)
+
+print(f"""{pink}
+
+To update the script 👇
+
+	
+
 rm -rf cod-filtering-keristofer 
 
 git clone https://github.com/arthur0010/cod-filtering-keristofer 
@@ -152,14 +262,16 @@ cd cod-filtering-keristofer
 
 chmod 777 * 
 
-python cod-filtering-keristofer.py
+python cod-filtering-keristofer.py""")
 
-""")
-print(" ")
-print(" ostore filtering ")
-print(" keristofer ")
-print(f"{blue} ")
-print("phone number = 09189256026 ")
-print(f"{pink} ")
-print("the end ")
-time.sleep(2.0)
+print("")
+
+sleep(1)
+
+print("")
+
+print("")
+
+print(text("                            THE END"))
+
+print("")
