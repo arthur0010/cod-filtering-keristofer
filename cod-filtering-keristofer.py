3060b2a2015422c47053b82f2b669c50d3c1ecd2
arthur0010 <94835248+arthur0010@users.noreply.github.com>
2023-05-16 23:30:04 +0330

    Add files via upload

---
+++ cod-filtering-keristofer.py
@@ -0,0 +1,148 @@
+from os import system
+system("pkg install figlet")
+try:
+	from datetime import datetime
+except:
+	system("pip install datetime")
+try:
+	from random import choice,randint
+except:
+	system("pip install random")
+try:
+	from time import sleep
+except:
+	system("pip install time")
+system("clear")
+
+green="\033[1;92m"
+red ="\033[1;91m"
+blue="\033[1;94m"
+pink="\033[1;95m"
+yellow="\033[1;93m"
+white="\033[1;00m"
+OF = "\033[0m"
+ww = "\033[0;100m"
+
+server = ["/y//d/f/","/f//d/","/d//f/h/","/f//a/y/","/e////f.h/","/f/h.u//","/f////g.h/","/g//d/","/f/h.g//","/r//g/h"]
+
+cod = ["1","2","3","4","5","6","7","8","9","0"]
+
+code = ("("+choice(cod)+"."+choice(cod)+"."+choice(cod)+"."+choice(cod)+choice(server)+choice(cod)+"."+choice(cod)+"."+choice(cod)+"."+choice(cod)+"."+choice(cod)+"."+choice(cod)+"."+choice(cod)+"."+choice(cod)+"."+choice(cod)+"."+choice(cod)+"."+choice(cod)+"."+choice(cod)+"."+choice(cod)+"."+choice(cod)+"."+choice(cod)+"."+choice(cod)+"."+choice(cod)+"."+choice(cod)+"."+choice(cod)+"."+choice(cod)+"."+choice(cod)+"."+choice(cod)+"."+choice(cod)+"."+choice(cod)+"."+choice(cod)+"."+choice(cod)+"."+choice(cod)+"."+choice(cod)+"."+choice(cod)+"."+choice(cod)+"."+choice(cod)+"."+choice(cod)+"."+choice(cod)+"."+choice(cod)+"."+choice(cod)+"."+choice(cod)+"."+choice(cod)+"/)")
+
+link_sexy = ["https://s8.uupload.ir/files/screenshot_۲۰۲۳۰۵۱۶_۱۶۲۵۳۱_v8p.jpg","https://s8.uupload.ir/files/screenshot_۲۰۲۳۰۵۱۶_۱۶۲۸۳۱_u05n.jpg","https://s8.uupload.ir/files/screenshot_۲۰۲۳۰۵۰۸_۱۴۵۰۱۴_uot3.jpg","https://s8.uupload.ir/files/crdghzedz1lx_6ew0.jpg"]
+
+
+n = 1
+r = "="
+
+while (n <= 100):
+	print(r,f"%{n}")
+	n = n + randint(1,5)
+	r = r + "="
+	sleep(000000.1)
+	
+sleep(3)
+system("clear")
+print(f"""{white}
+
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⡠⢤⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⡴⠟⠃⠀⠀⠙⣄⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣠⠋⠀⠀⠀⠀⠀⠀⠘⣆⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢠⠾⢛⠒⠀⠀⠀⠀⠀⠀⠀⢸⡆⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣿⣶⣄⡈⠓⢄⠠⡀⠀⠀⠀⣄⣷⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣿⣷⠀⠈⠱⡄⠑⣌⠆⠀⠀⡜⢻⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⣿⡿⠳⡆⠐⢿⣆⠈⢿⠀⠀⡇⠘⡆⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢿⣿⣷⡇⠀⠀⠈⢆⠈⠆⢸⠀⠀⢣⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠘⣿⣿⣿⣧⠀⠀⠈⢂⠀⡇⠀⠀⢨⠓⣄⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣸⣿⣿⣿⣦⣤⠖⡏⡸⠀⣀⡴⠋⠀⠈⠢⡀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢠⣾⠁⣹⣿⣿⣿⣷⣾⠽⠖⠊⢹⣀⠄⠀⠀⠀⠈⢣⡀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡟⣇⣰⢫⢻⢉⠉⠀⣿⡆⠀⠀⡸⡏⠀⠀⠀⠀⠀⠀⢇
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢨⡇⡇⠈⢸⢸⢸⠀⠀⡇⡇⠀⠀⠁⠻⡄⡠⠂⠀⠀⠀⠘
+⢤⣄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢠⠛⠓⡇⠀⠸⡆⢸⠀⢠⣿⠀⠀⠀⠀⣰⣿⣵⡆⠀⠀⠀⠀
+⠈⢻⣷⣦⣀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣠⡿⣦⣀⡇⠀⢧⡇⠀⠀⢺⡟⠀⠀⠀⢰⠉⣰⠟⠊⣠⠂⠀⡸
+⠀⠀⢻⣿⣿⣷⣦⣀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣠⢧⡙⠺⠿⡇⠀⠘⠇⠀⠀⢸⣧⠀⠀⢠⠃⣾⣌⠉⠩⠭⠍⣉⡇
+⠀⠀⠀⠻⣿⣿⣿⣿⣿⣦⣀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣠⣞⣋⠀⠈⠀⡳⣧⠀⠀⠀⠀⠀⢸⡏⠀⠀⡞⢰⠉⠉⠉⠉⠉⠓⢻⠃
+⠀⠀⠀⠀⠹⣿⣿⣿⣿⣿⣿⣷⡄⠀⠀⢀⣀⠠⠤⣤⣤⠤⠞⠓⢠⠈⡆⠀⢣⣸⣾⠆⠀⠀⠀⠀⠀⢀⣀⡼⠁⡿⠈⣉⣉⣒⡒⠢⡼⠀
+⠀⠀⠀⠀⠀⠘⣿⣿⣿⣿⣿⣿⣿⣎⣽⣶⣤⡶⢋⣤⠃⣠⡦⢀⡼⢦⣾⡤⠚⣟⣁⣀⣀⣀⣀⠀⣀⣈⣀⣠⣾⣅⠀⠑⠂⠤⠌⣩⡇⠀
+⠀⠀⠀⠀⠀⠀⠘⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡁⣺⢁⣞⣉⡴⠟⡀⠀⠀⠀⠁⠸⡅⠀⠈⢷⠈⠏⠙⠀⢹⡛⠀⢉⠀⠀⠀⣀⣀⣼⡇⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠈⠻⣿⣿⣿⣿⣿⣿⣿⣿⣽⣿⡟⢡⠖⣡⡴⠂⣀⣀⣀⣰⣁⣀⣀⣸⠀⠀⠀⠀⠈⠁⠀⠀⠈⠀⣠⠜⠋⣠⠁⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠙⢿⣿⣿⣿⡟⢿⣿⣿⣷⡟⢋⣥⣖⣉⠀⠈⢁⡀⠤⠚⠿⣷⡦⢀⣠⣀⠢⣄⣀⡠⠔⠋⠁⠀⣼⠃⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠻⣿⣿⡄⠈⠻⣿⣿⢿⣛⣩⠤⠒⠉⠁⠀⠀⠀⠀⠀⠉⠒⢤⡀⠉⠁⠀⠀⠀⠀⠀⢀⡿⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠙⢿⣤⣤⠴⠟⠋⠉⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠑⠤⠀⠀⠀⠀⠀⢩⠇⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+
+""")
+sleep(1)
+print("")
+print(f"            {red}✪ {green}START TIME --->",datetime.now().strftime('%Y-%m-%d  %H:%M:%S'),f"{red}✪ ")
+
+sleep(1)
+print("")
+print("")
+print(f"{yellow}―――‣{pink} im keristofer")
+sleep(0.4)
+print(f"{yellow}―――――‣{pink} rekord dar filtering ")
+sleep(0.3)
+print(f"{yellow}―――――――‣{pink} 09189256026")
+sleep(0.2)
+print(blue)
+system("figlet keristofer")
+print("")
+print(f"{red}[{green}1{red}] {yellow}―――‣ {red}code account ")
+print(f"{red}[{green}2{red}] {yellow}―――‣ {red}code group ")
+print(f"{red}[{green}3{red}] {yellow}―――‣ {red}code channel ")
+print(f"{red}[{green}4{red}] {yellow}―――‣ {red}link sexy ")
+print(f"{red}[{green}5{red}] {yellow}―――‣ {red}link mokharab ")
+print(f"{red}[{green}6{red}] {yellow}―――‣ {red}code hasas be server ")
+print(f"{red}[{green}7{red}] {yellow}―――‣ {red}code tor ")
+print(f"{red}[{green}8{red}] {yellow}―――‣ {red}link virus ")
+print(f"{red}[{green}9{red}] {yellow}―――‣ {red}ip rubika ")
+print("")
+ii = int(input(f"{red}[{green}?{red}] {green}Please enter the desired option --->{white} "))
+sleep(2)
+print("")
+print("")
+if ii == 1:
+	print(f"{blue} code account : {ww}{code} ",OF)
+if ii == 2:
+	print(f"{blue} code group : {ww}{code} ",OF)
+if ii == 3:
+	print(f"{blue} code channel : {ww}{code} ",OF)
+if ii == 4:
+	print(f"{blue} link sexy : {ww}{(choice(link_sexy))} ",OF)
+if ii == 5:
+	print(f"{blue} link mokharab : {ww}https://bit.ly/3ild93L ",OF)
+if ii == 6:
+	print(f"{blue} code hasas be server : {ww}(Server.onlline/server.offline.onlline/)",OF)
+
+tor = datetime.now().strftime('%d  %H:%M:%S')
+if ii == 7:
+	print(f"{blue} code tor : {ww}{tor} ",OF)
+if ii == 8:	
+	print(f"{blue} link virus : {ww}http://Hackweb.xyz/python.html.cs ",OF)
+if ii == 9:
+	print(f"{blue} ip rubika : {ww}5.106.8.151 ",OF)
+sleep(3)
+print("")
+print("")
+print(f"                  \033[0;102m {red} makhlot konid bad bezanid{OF}")
+sleep(1)
+print(f"""{pink}
+To update the script 👇 
+	
+rm -rf cod-filtering-keristofer 
+
+git clone https://github.com/arthur0010/cod-filtering-keristofer 
+
+cd cod-filtering-keristofer 
+
+chmod 777 * 
+
+python cod-filtering-keristofer.py""")
+print("")
+sleep(1)
+print("")
+print("")
+print(white)
+print("                            THE END")
+print("")
